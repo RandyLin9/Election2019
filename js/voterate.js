3e59b28955023760e1e33366c8397e97fa72e8fa
RandyLin9 <zhihuairandy@gmail.com>
2019-12-19 15:51:07 +0800

    V1

--- js/voterate.js
+++ js/voterate.js
@@ -1,18 +1,18 @@
-var tearGasOptions = {
+var voterateOptions = {
     visualMap: {
-        min: 20,
-        max: 500,
+        min: 0,
+        max: 100,
         text: ['High', 'Low'],
         realtime: false,
         calculable: true,
         inRange: {
-            color: ['lightskyblue', 'red']
+            color: ['#4557c7','#5968cd','#6c7ad3','#808cd9','#949edf','#a7b0e5','#bbc1eb', '#ebe4bb','#d9cd80','#c7b645']
         }
     },
 
     tooltip: {
         trigger: 'item',
-        formatter: '{b} - {c}次'
+        formatter: '{b} - {c}%'
     },
     series: [{
         name: 'District Council',
@@ -46,26 +46,25 @@ var tearGasOptions = {
                 },
                 }
             },
-            
         data: [
-            { name: '中西區', value: 20 },
-            { name: '灣仔', value: 50 },
-            { name: '東區', value: 10 },
-            { name: '南區', value: 12 },
-            { name: '油尖旺', value: 500 },
-            { name: '深水埗', value: 300 },
-            { name: '九龍城', value: 100 },
-            { name: '黃大仙', value: 300 },
-            { name: '觀塘', value: 50 },
-            { name: '葵青', value: 120 },
-            { name: '荃灣', value: 360 },
-            { name: '屯門', value: 180 },
-            { name: '元朗', value: 240 },
-            { name: '北區', value: 30 },
-            { name: '大埔', value: 110 },
-            { name: '沙田', value: 1200 },
-            { name: '西貢', value: 180 },
-            { name: '離島', value: 20 }
+            { name: '中西區', value: 93 },
+            { name: '灣仔', value: 69 },
+            { name: '東區', value: 91 },
+            { name: '南區', value: 88 },
+            { name: '油尖旺', value: 85 },
+            { name: '深水埗', value: 88 },
+            { name: '九龍城', value: 60 },
+            { name: '黃大仙', value: 100 },
+            { name: '觀塘', value: 70 },
+            { name: '葵青', value: 87 },
+            { name: '荃灣', value: 84 },
+            { name: '屯門', value: 90 },
+            { name: '元朗', value: 84 },
+            { name: '北區', value: 83 },
+            { name: '大埔', value: 100 },
+            { name: '沙田', value: 97 },
+            { name: '西貢', value: 89 },
+            { name: '離島', value: 70 }
         ],
         nameMap: {
             'Central and Western': '中西區',
